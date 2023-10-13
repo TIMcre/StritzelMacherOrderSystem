58abdd5cc182c1abb6287bfe6bac54f10fe9386f
TIMcre <92308299+TIMcre@users.noreply.github.com>
2023-10-13 13:41:53 +0000

    new file:   .devcontainer/devcontainer.json modified:   .gitignore new file:   order_history.csv modified:   system.py

--- system.py
+++ system.py
@@ -1,35 +1,68 @@
 import sys
-
-
+import re
+import time
 
 strizel_sorts = {
-  1 : "Zimt und Zucker"
-  2 : "Oreo"
-  
+    0: "Vanille Zucker",
+    1: "Zimt und Zucker",
+    2: "Oreo",
+    3: "",
+    4: "",
+    5: "",
+    6: "",
+    7: "",
+    8: "",
+    9: "",
+    10: "",
+    11: "",
+    12: "",
 }
+
+stritzel_insert_sorts = {
+    0: "h",
+    1: "Nutella",
+    2: "Weiße Schokoloade",
+    3: "Apfelmus",
+}
+
+
 def get_input():
-  
+    while True:
+        code = input("Button Code: ")
+        # if re.match(code, r""):
+        return code
+
+
 def convert_to_stritzel(number):
-  try:
     stritzel = stritzel_sorts[number]
-  except ValueError:
-    sys.exit("value error")
 
-  return strizel
+    return stritzel
 
-def add_order():
 
-def remove_order():
+def add_order(order_list, order):
+    order_list.append(order)
+    return order_list
 
-def add_history():
+
+def remove_order(order_list, order_number=0):
+    removed_order = order_list.pop(order_number)
+    return {"list": order_list, "order": removed_order}
+
+
+def add_history(order, file="order_history.csv"):
+    with open(file, "a"):
+        file.write([order["number"], order["insert"]])
+
 
 def main():
-  get_input()
-  strizel = convert_to_strizel(input)
-  add_order(strizel)
-  # if 2nd input true remove_order and add to history
+    order_list = []
+    input_code = get_input()
+    if input_code == "rm":
+        remove_order(order_list)
+    strizel = convert_to_strizel(input)
+    add_order(strizel)
+    # if 2nd input true remove_order and add to history
 
 
 if __name__ == "__main__":
-  main()
-  
+    main()
