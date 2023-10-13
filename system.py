49a7a92e38a974d9842c66d7a08e9501131a99b1
TIMcre <92308299+TIMcre@users.noreply.github.com>
2023-10-13 15:51:20 +0000

    fixed everything

--- system.py
+++ system.py
@@ -1,67 +1,114 @@
+stritzel_outside_sorts = {
+    "0": "Vanille Zucker",
+    "1": "Zimt und Zucker",
+    "2": "Oreo",
+    "3": "",
+    "4": "",
+    "5": "",
+    "6": "",
+    "7": "",
+    "8": "",
+    "9": "",
+    "10": "",
+    "11": "",
+    "12": "",
+}
+
+stritzel_inside_sorts = {
+    "0": "",
+    "1": "Nutella",
+    "2": "Weiße Schokoloade",
+    "3": "Apfelmus",
+}
+
+# -----------------------------------
 import sys
 import re
 import time
 
-strizel_sorts = {
-    0: "Vanille Zucker",
-    1: "Zimt und Zucker",
-    2: "Oreo",
-    3: "",
-    4: "",
-    5: "",
-    6: "",
-    7: "",
-    8: "",
-    9: "",
-    10: "",
-    11: "",
-    12: "",
-}
 
-stritzel_insert_sorts = {
-    0: "h",
-    1: "Nutella",
-    2: "Weiße Schokoloade",
-    3: "Apfelmus",
-}
+def convert_to_name(outside, inside):
+    outside_name = stritzel_outside_sorts[outside]
+    inside_name = stritzel_inside_sorts[inside]
 
-
-def get_input():
-    while True:
-        code = input("Button Code: ")
-        # if re.match(code, r""):
-        return code
-
-
-def convert_to_stritzel(number):
-    stritzel = stritzel_sorts[number]
-
-    return stritzel
+    return {
+        "outside": outside_name,
+        "inside": inside_name,
+    }
 
 
 def add_order(order_list, order):
     order_list.append(order)
+
     return order_list
 
 
 def remove_order(order_list, order_number=0):
-    removed_order = order_list.pop(order_number)
+    try:
+        removed_order = order_list.pop(order_number)
+    except:
+        return None
+        
     return {"list": order_list, "order": removed_order}
 
 
 def add_history(order, file="order_history.csv"):
     with open(file, "a"):
-        file.write([order["number"], order["insert"]])
+        file.write([order["number"], order["inside"]])
+
+
+def print_orders(orders, mode):
+    print()
+    if mode == "production":
+        for order in orders:
+            print(f'{order["name"]["outside"]}')
+    elif mode == "serving":
+        for order in order:
+            print(f'Outside: {order["name"]["outside"]} Inside: {order["name"]["inside"]}')
+
+
+def is_valid_code(code):
+    if re.match(r"((0[0-9])|[1-9]{2})[0-3]", code) or code == "rm":
+        return True
+    return False
+
+
+def convert_code_to_order(code):
+    outside_sort = "0" if code[:2] == "00" else code[:2].lstrip("0")
+    inside_sort = code[2:]
+    name = convert_to_name(outside_sort, inside_sort)
+
+    return {
+        "outside_sort": outside_sort,
+        "inside_sort": inside_sort,
+        "name": name,
+    }
 
 
 def main():
     order_list = []
-    input_code = get_input()
-    if input_code == "rm":
-        remove_order(order_list)
-    strizel = convert_to_strizel(input)
-    add_order(strizel)
-    # if 2nd input true remove_order and add to history
+
+    while True:
+        try:
+            input_code = str(input("Code: ")).strip()
+
+            if not is_valid_code(input_code):
+                raise ValueError
+
+            if input_code == "rm":
+                remove_order(order_list)
+
+            else:
+                order = convert_code_to_order(input_code)
+                order_list = add_order(order_list, order)
+
+            print_orders(order_list, "production")
+
+        except ValueError:
+            pass
+
+        except KeyboardInterrupt:
+            sys.exit("\n--Ended by user--")
 
 
 if __name__ == "__main__":
